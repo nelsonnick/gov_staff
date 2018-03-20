00b759aaaa9aa391686ad6a09de520fe78894157
Administrator <nelsonnick@126.com>
2018-03-20 17:01:34 +0800

    重构

--- qingdao.py
+++ qingdao.py
@@ -6,28 +6,6 @@ import re
 import urllib.parse
 import pymysql
 
-# 槐荫区：单位、姓名、性别
-# 市中区：单位、部门、姓名、性别
-# 历下区：单位、姓名、性别、占用编制情况
-# 历城区：单位、姓名、性别
-# 天桥区：单位、姓名、性别
-# 章丘区：单位、姓名、性别
-# 市直：单位、姓名、性别、占用编制情况
-
-Dict = {
-    '槐荫': 'hy.',
-    '历下': 'lx.',
-    '历城': 'lc.',
-    '商河': 'sh.',
-    '天桥': 'tq.',
-    '市中': 'sz.',
-    '市直': '',
-    '平阴': 'py.',
-    '济阳': 'jy.',
-    '章丘': 'zq.',
-    '长清': 'cq.'
-        }
-
 
 class Person:
     def __init__(self, dw, xm, xb, bm, zybzqk):
@@ -106,9 +84,9 @@ def save_person(person, dwzd, dwbh, bzlx):
     db.close()
 
 
-def person_info(dwbh, bzlx, dwzd):
+def down_person(dwmc, dwbh, bzlx, dwzd):
     rt = requests.get(
-        "http://" + Dict[dwzd] + "jnbb.gov.cn/smzgs/PersonList.aspx?unitId=" + dwbh + "&BZLX=" + bzlx, timeout=60)
+        "http://120.221.95.1:1888/PersonList.aspx?unitId=" + dwbh + "&BZLX=" + bzlx, timeout=60)
     key = rt.text
     titles = re.findall(r'<th.+?</th>', key)
     cols = []
@@ -119,23 +97,28 @@ def person_info(dwbh, bzlx, dwzd):
         for person in persons:
             information = re.findall(r'<td>.+?</td>', person)
             save_person(get_person_info(cols, information), dwzd, dwbh, bzlx)
+        print(dwzd + ':' + dwbh + '-' + dwmc + '-' + bzlx + '--->下载完成！')
     elif len(cols) == 4:
         persons = re.findall(r'<td>.+?</td><td>.+?</td><td>.+?</td><td>.+?</td>', key)
         for person in persons:
             information = re.findall(r'<td>.+?</td>', person)
             save_person(get_person_info(cols, information), dwzd, dwbh, bzlx)
+        print(dwzd + ':' + dwbh + '-' + dwmc + '-' + bzlx + '--->下载完成！')
     elif len(cols) == 5:
         persons = re.findall(r'<td>.+?</td><td>.+?</td><td>.+?</td><td>.+?</td><td>.+?</td>', key)
         for person in persons:
             information = re.findall(r'<td>.+?</td>', person)
             save_person(get_person_info(cols, information), dwzd, dwbh, bzlx)
+        print(dwzd + ':' + dwbh + '-' + dwmc + '-' + bzlx + '--->下载完成！')
+    elif len(cols) == 0:
+        print(dwzd + ':' + dwbh + '-' + dwmc + '-' + bzlx + '--->无人员信息！')
     else:
-        print('出现六列表格，无法识别')
+        print(dwzd + ':' + dwbh + '-' + dwmc + '-' + bzlx + '--->无法识别！')
 
 
 def get_department(dwzd):
     browser = webdriver.Chrome()
-    browser.get("http://" + Dict[dwzd] + "jnbb.gov.cn/smzgs/TreeViewPage.aspx")
+    browser.get("http://120.221.95.1:1888/TreeViewPage.aspx?type=dep")
     t = browser.page_source
     browser.close()
     department_string = re.compile(r"ipt:f\(.+?\);\" title=").findall(t)
@@ -143,13 +126,13 @@ def get_department(dwzd):
     for d in department_string:
         code = d.split('\'')[1]
         name = urllib.parse.unquote(d.split('\'')[3])
-        department_info(code, dwzd)
+        down_department(code, dwzd)
         file.write(code + "\t" + name + "\n")
     file.close()
 
 
-def department_info(dwbh, dwzd):
-    rt = requests.get("http://" + Dict[dwzd] + "jnbb.gov.cn/smzgs/UnitDetails.aspx?unitId=" + dwbh, timeout=60)
+def down_department(dwbh, dwzd):
+    rt = requests.get("http://120.221.95.1:1888/UnitDetails.aspx?unitId=" + dwbh, timeout=60)
     key = rt.text
     # 去掉全部的空格
     keys = re.compile(r' ').sub('', key)
@@ -245,7 +228,7 @@ def department_info(dwbh, dwzd):
         patternH = re.compile(r"BZLX=.+?'style='")
         nameH = re.search(patternH, name).group(0)
         bzlx = nameH[5:len(nameH) - 8]
-        person_info(dwbh, bzlx, dwzd)
+        down_person(dwmc, dwbh, bzlx, dwzd)
     # 定位到编制数
     nameJJ = re.findall(r'<tdwidth="18%">.+?</a>名\)</td>', keys)
     for name in nameJJ:
@@ -288,7 +271,7 @@ def department_info(dwbh, dwzd):
         patternO = re.compile(r"BZLX=.+?'style='")
         nameO = re.search(patternO, name).group(0)
         bzlx = nameO[5:len(nameO) - 8]
-        person_info(dwbh, bzlx, dwzd)
+        down_person(dwmc, dwbh, bzlx, dwzd)
     db = pymysql.connect("localhost", "root", "root", "bz", charset='utf8')
     cursor = db.cursor()
     sql = "INSERT INTO department(dwzd, dwbh, dwmc, qtmc, ldzs, jb, nsjg, xz_bzs, xz_sjs, sy_bzs, sy_sjs, gq_bzs, gq_sjs) \
@@ -300,12 +283,7 @@ def department_info(dwbh, dwzd):
     except:
         db.rollback()
     db.close()
-    print(dwzd + ":" + dwbh + "下载完成！")
+    # print(dwzd + ':' + dwbh + '-' + dwmc + '下载完成！')
 
 
-def down():
-    for location in Dict:
-        get_department(location)
-
-
-down()
+get_department('青岛')
