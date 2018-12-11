12d7d8a133e2ec7c0ac0a0380351a8542edc4a55
nelsonnick <nelsonnick@126.com>
2018-12-12 07:51:42 +0800

    。

--- location/JiNan.py
+++ location/JiNan.py
@@ -262,37 +262,106 @@ def down_structure_one(dwzd):
 # 下载单位结构
 # 参数：单位驻地
 def down_structure_all():
-    file = open("c:\\济南.txt", "a", encoding='UTF-8')
+    file = open("d:\\济南.txt", "a", encoding='UTF-8')
     for dwzd in Dict:
         file.write(down_structure_one(dwzd))
         print(dwzd + '：已下载完成！')
     file.close()
 
 
-city = district = category = type = ''
-for line in open("c:\\济南.txt", "r", encoding='UTF-8'):
+city = district = category = type = dwbh = dwmc = ''
+num = 0
+for line in open("d:\\济南.txt", "r", encoding='UTF-8'):
     if line == '\t济南市\n':
-        city = line
-        print(city)
+        city = '济南市'
         continue
     if line == '\t\t市直\n':
-        district = line
-        print(district)
+        district = '市直'
+        continue
+    if line == '\t\t市中区\n':
+        district = '市中区'
+        continue
+    if line == '\t\t历下区\n':
+        district = '历下区'
+        continue
+    if line == '\t\t槐荫区\n':
+        district = '槐荫区'
+        continue
+    if line == '\t\t天桥区\n':
+        district = '天桥区'
+        continue
+    if line == '\t\t历城区\n':
+        district = '历城区'
+        continue
+    if line == '\t\t长清区\n':
+        district = '长清区'
+        continue
+    if line == '\t\t章丘区\n':
+        district = '章丘区'
+        continue
+    if line == '\t\t济阳县\n':
+        district = '济阳县'
+        continue
+    if line == '\t\t商河县\n':
+        district = '商河县'
+        continue
+    if line == '\t\t平阴县\n':
+        district = '平阴县'
         continue
     if line == '\t\t\t党委\n':
-        category = line
-        print(category)
-        continue
-    if line == '\t\t\t\t行政机关\n':
-        type = line
-        print(type)
-        continue
-    if line == '\t\t\t\t直属事业单位\n':
-        type = line
-        print(type)
+        category = '党委'
         continue
     if line == '\t\t\t人大\n':
-        category = line
-        print(category)
+        category = '人大'
         continue
+    if line == '\t\t\t政府\n':
+        category = '政府'
+        continue
+    if line == '\t\t\t政协\n':
+        category = '政协'
+        continue
+    if line == '\t\t\t民主党派\n':
+        category = '民主党派'
+        continue
+    if line == '\t\t\t群众团体\n':
+        category = '群众团体'
+        continue
+    if line == '\t\t\t法院\n':
+        category = '法院'
+        continue
+    if line == '\t\t\t检察院\n':
+        category = '检察院'
+        continue
+    if line == '\t\t\t经济实体\n':
+        category = '经济实体'
+        continue
+    if line == '\t\t\t街道办事处\n':
+        category = '街道办事处'
+        continue
+    if line == '\t\t\t乡\n':
+        category = '乡'
+        continue
+    if line == '\t\t\t镇\n':
+        category = '镇'
+        continue
+    if line == '\t\t\t\t行政机关\n':
+        type = '行政机关'
+        continue
+    if line == '\t\t\t\t直属事业单位\n':
+        type = '事业单位'
+        continue
+    if line == '\t\t\t\t\t\t下设机构\n':
+        type = '行政机关'
+        continue
+    if line == '\t\t\t\t\t\t事业单位\n':
+        type = '事业单位'
+        continue
+    if line.count('\t') > num:
+        num = line.count('\t')
+    else:
+        num = 0
 
+    co = line.replace('\t', '').replace('\n', '')
+    dwbh = co.split("-")[0]
+    dwmc = co.split("-")[1]
+    print(city + '-' + district + '-' + category + '-' + type + '-' + dwbh + '-' + dwmc)
